de4d17fc8279e0f0a3d317bda0f08587bf8612bd
Jhonathan Angus <jhonathan.angus@gmail.com>
2019-08-03 18:46:52 -0700

    add expenses summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,13 +4,17 @@ import numeral from 'numeral';
 import getVisibleExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/selectExpensesTotal';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => (
-  <p>
-  Viewing {expenseCount}
-  {expenseCount === 1 ? " expense " : " expenses "}
-  totalling {numeral(expensesTotal / 100).format('$0,0.00')}
-  </p>
-);
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? " expense " : " expenses ";
+  const total = numeral(expensesTotal / 100).format('$0,0.00');
+  return (
+    <div>
+      <h3>
+        Viewing {expenseCount} {expenseWord} totalling {total}
+      </h3>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   const expenses = getVisibleExpenses(state.expenses, state.filters);
