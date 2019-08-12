0b96194817ede0282bde219bcfdeea02a41abf0e
Jhonathan Angus <jhonathan.angus@yahoo.com>
2019-08-12 18:25:33 +0000

    setup test database env

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,5 +1,9 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
+
+// required for process.env firebase variables in test mode
+DotEnv.config({ path: '.env.test' });
 
 Enzyme.configure({
   adapter: new Adapter()
