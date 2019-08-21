1d2aa90cecc47dc4bb06524c4548f9828351086d
Jhonathan Angus <jhonathan.angus@yahoo.com>
2019-08-21 21:10:35 +0000

    styled the login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,11 +3,16 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button
-            onClick={startLogin}>
-                Login
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expendify</h1>
+            <p>Track, search, and expect what you're spending.</p>
+            <button 
+                className="button"
+                onClick={startLogin}>
+                    Login with Google
             </button>
+        </div>
     </div>
 );
 
